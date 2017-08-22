f427a8677c9f0cab5acb7a5c92044f750b48a529
ILSHATGIZATULIN <gizatulin86@gmail.com>
2017-08-22 19:28:28 +0300

    включили css

--- index.php
+++ index.php
@@ -2,25 +2,32 @@
 //echo '<pre>';
 //var_dump($_REQUEST);
 
-$message= false;
+$message= false;//
+$error= false ;// для ошибок
 
-if (isset($_REQUEST [ 'name']) and isset ($_REQUEST['phone'])) {
-
-    $name = $_REQUEST ['name'];
+if (isset($_REQUEST['name']) and isset ($_REQUEST['phone'])) {
+    $name = $_REQUEST['name'];
     $phone = $_REQUEST['phone'];
+    echo 1;
 
-    $row = 'здравтсвуйте,' . $name .
-        'Ваш номер:' . $phone . PHP_EOL;
-    file_put_contents('./contacts.txt',
-        $row,
-        FILE_APPEND);
+    if (empty($name) || empty($phone)) {
+        ;$error= ' Заполните поля';
+    } else {
+        $row = 'здравтсвуйте,' . $name .
+            'Ваш номер:' . $phone . PHP_EOL;
 
 
-    $message = 'Спасибо мы с вами свяжемся.';
+        file_put_contents('./contacts.txt',
+            $row,
+            FILE_APPEND);
+        $message = 'Спасибо мы с вами свяжемся.';
+        echo 2;
+    }
+
+
+
+
 }
-
-
-
 
 
 ?>
@@ -34,22 +41,49 @@ if (isset($_REQUEST [ 'name']) and isset ($_REQUEST['phone'])) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="bootstrap.min.css.">
 </head>
 <body>
+<div class="container">
+
+
+    <h3> Форма обратной связи</h3>
 <?php if ($message): ?>
 <?=$message ?>
     <?php else: ?>
-<form action="index.php" metod="post">
-<p>представтесь</p>
-<input type ="text"  name ="name">
-       <p>Укажите ваш номер</p>
-<input type ="text" name="phone">
-    <button type="submit">Отправить</button>
-</form>
-<?php endif; ?>
+
+
+
+
+
+<form class="form-horizontal" role="form" method="post">
+    <div class="form-group">
+        <label for="name" class="col-sm-2 control-label">Имя</label>
+        <div class="col-sm-10">
+            <input type="text" name="name" class="form-control"  placeholder="Введите имя">
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="phone" class="col-sm-2 control-label">Номер телефона</label>
+        <div class="col-sm-10">
+            <input type="text" name="phone" class="form-control"  placeholder="Введите номер">
+        </div>
+    </div>
+
+                </label>
+            </div>
+        </div>
+    </div>
 
 </body>
 
-</html>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-primary">Отправить</button>
+        </div>
+        <p class ="alert-danger col-sm-4"><?=$error ?></p>
+    </div>
+</form>
 
+<?php endif; ?>
 
