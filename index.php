018f8b161874f22b634269d871b8b61b7a3df9ad
ILSHATGIZATULIN <gizatulin86@gmail.com>
2017-08-22 22:47:45 +0300

    Исправленная дома версия с css

--- index.php
+++ index.php
@@ -1,89 +1,76 @@
 <?php
 //echo '<pre>';
 //var_dump($_REQUEST);
-
 $message= false;//
 $error= false ;// для ошибок
-
 if (isset($_REQUEST['name']) and isset ($_REQUEST['phone'])) {
     $name = $_REQUEST['name'];
     $phone = $_REQUEST['phone'];
-    echo 1;
 
     if (empty($name) || empty($phone)) {
         ;$error= ' Заполните поля';
     } else {
         $row = 'здравтсвуйте,' . $name .
             'Ваш номер:' . $phone . PHP_EOL;
-
-
         file_put_contents('./contacts.txt',
             $row,
             FILE_APPEND);
         $message = 'Спасибо мы с вами свяжемся.';
-        echo 2;
+
     }
-
-
-
-
 }
-
-
 ?>
 
 <!doctype html>
 
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="bootstrap.min.css.">
+    <link rel="stylesheet" href="./css/bootstrap.min.css.">
 </head>
 <body>
 <div class="container">
 
 
     <h3> Форма обратной связи</h3>
-<?php if ($message): ?>
-<?=$message ?>
+    <?php if ($message): ?>
+
+        <?=$message?>
+
     <?php else: ?>
 
 
 
 
 
-<form class="form-horizontal" role="form" method="post">
-    <div class="form-group">
-        <label for="name" class="col-sm-2 control-label">Имя</label>
-        <div class="col-sm-10">
-            <input type="text" name="name" class="form-control"  placeholder="Введите имя">
-        </div>
-    </div>
-    <div class="form-group">
-        <label for="phone" class="col-sm-2 control-label">Номер телефона</label>
-        <div class="col-sm-10">
-            <input type="text" name="phone" class="form-control"  placeholder="Введите номер">
-        </div>
-    </div>
-
-                </label>
+    <form class="form-horizontal" role="form" action= "index.php" method="post">
+        <div class="form-group">
+            <label for="name" class="col-sm-2 control-label">Имя</label>
+            <div class="col-sm-10">
+                <input type="text" name="name" class="form-control"  placeholder="Введите имя">
             </div>
         </div>
-    </div>
-
-</body>
-
-    <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-primary">Отправить</button>
+        <div class="form-group">
+            <label for="phone" class="col-sm-2 control-label">Номер телефона</label>
+            <div class="col-sm-10">
+                <input type="text" name="phone" class="form-control"  placeholder="Введите номер">
+            </div>
         </div>
-        <p class ="alert-danger col-sm-4"><?=$error ?></p>
+
+
+<div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-primary">Отправить</button>
     </div>
+    <p class ="alert-danger col-sm-4"><?=$error ?></p>
+</div>
 </form>
 
 <?php endif; ?>
+</body>
+</html>
 
